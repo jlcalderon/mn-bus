a12dbb4e38da1f74a578f3d9f55139927889ec0d
jlcalderon <jlcalderonfuentes@gmail.com>
2021-05-08 22:11:43 -0500

    Grabbing user's route selection on Home component state

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,6 +3,11 @@ import Header from "../../components/header/Header";
 import "../../App.css";
 const Home = () => {
   const [routes, setRoutes] = useState([]);
+  const [usrRoute, setUsrRoute] = useState(0);
+
+  useEffect(() => {
+    console.log(usrRoute);
+  }, [usrRoute]);
 
   useEffect(() => {
     fetch("https://svc.metrotransit.org/nextrip/Routes?format=json")
@@ -16,7 +21,13 @@ const Home = () => {
       <div className="forms-container">
         <h1>1. Select a Route to Travel</h1>
         <form className="forms">
-          <select className="form-control">
+          <select
+            className="form-control"
+            onChange={(event) => {
+              event.preventDefault();
+              setUsrRoute(event.target[event.target.selectedIndex].value);
+            }}
+          >
             <option key="0" value="0">
               Choose a route
             </option>
