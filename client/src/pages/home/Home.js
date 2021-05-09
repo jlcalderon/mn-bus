7995fc789bf4d7c2ffa76087fdb1c100ba6b56bc
jlcalderon <jlcalderonfuentes@gmail.com>
2021-05-08 21:36:26 -0500

    Retrieving routes from metrotransit API

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import "../../App.css";
 const Home = () => {
+  const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://svc.metrotransit.org/nextrip/Routes?format=json")
+      .then((response) => response.json())
+      .then((data) => setRoutes(data));
+  }, []);
+
   return (
     <div>
       <Header text="Welcome to MN Bus find out where to go to ride in the bus." />
@@ -9,7 +17,14 @@ const Home = () => {
         <h1>1. Select a Route to Travel</h1>
         <form className="forms">
           <select className="form-control">
-            <option value="0">Choose a route</option>
+            <option key="0" value="0">
+              Choose a route
+            </option>
+            {routes.map((route) => (
+              <option key={route.Route} value={route.Route}>
+                {route.Description}
+              </option>
+            ))}
           </select>
           <button className="form-control" type="submit">
             NEXT >
